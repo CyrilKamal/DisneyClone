25bac6a747c7bee41347efd4edbf199fd2d2cc86
CyrilKamal <43249970+CyrilKamal@users.noreply.github.com>
2021-10-31 00:19:17 -0400

    Added Navbar with Highlight on hover home icon

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,12 @@ const Login = (props) => {
             <CTA>
               <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
               <SignUp>GET ALL THERE</SignUp>
+              <Description>
+                  Get Premier Access to Raya and the Last Dragon for an additional fee 
+                  with a Disney+ subscription. As of 3/16/21, the price of Disney+ 
+                  and The Disney Bundle will increase by $1.
+              </Description>
+              <CTALogoTwo src="/images/cta-logo-two.png" alt=""/>
             </CTA>
             <BgImage />
         </Content>
@@ -55,7 +61,7 @@ max-width: 650px;
 width: 100%;
 display: flex;
 flex-direction: column;
-`
+`;
 
 const CTALogoOne = styled.img`
 margin-bottom: 12px;
@@ -63,7 +69,7 @@ max-width: 660px;
 min-height: 1px;
 display: block;
 width: 100%;
-`
+`;
 
 const SignUp = styled.a`
 font-weight: bold;
@@ -80,5 +86,21 @@ border-radius: 4px;
     &:hover {
         background-color: #0483ee;
     }
+`;
+
+const Description = styled.p`
+color: hsla(0, 0%, 95.3%, 1);
+font-size: 11px;
+margin: 0 0 24px;
+line-height: 1.5;
+letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+max-width: 600px;
+margin-bottom: 20px;
+display: inline-block;
+vertical-align: bottom;
+width: 100%;
 `
 export default Login;
