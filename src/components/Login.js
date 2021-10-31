c614a97650cde6d7d9ade3050f89c978aff8bf7c
CyrilKamal <43249970+CyrilKamal@users.noreply.github.com>
2021-10-30 21:54:40 -0400

    Added Background, Logos, and Interactive button

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,13 @@ import styled from "styled-components";
 const Login = (props) => {
     return(
     <Container>
-        <Content>Content</Content>
+        <Content>
+            <CTA>
+              <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+              <SignUp>GET ALL THERE</SignUp>
+            </CTA>
+            <BgImage />
+        </Content>
     </Container>
     );
 };
@@ -31,4 +37,48 @@ height: 100%;
 
 `;
 
+const BgImage = styled.div`
+height: 100%;
+background-position: top;
+background-size: cover;
+background-repeat: no-repeat;
+background-image: url("/images/login-background.jpg");
+position: absolute;
+top: 0;
+right: 0;
+left: 0;
+z-index: -1;
+`;
+
+const CTA = styled.div`
+max-width: 650px;
+width: 100%;
+display: flex;
+flex-direction: column;
+`
+
+const CTALogoOne = styled.img`
+margin-bottom: 12px;
+max-width: 660px;
+min-height: 1px;
+display: block;
+width: 100%;
+`
+
+const SignUp = styled.a`
+font-weight: bold;
+color: #f9f9f9;
+background-color: #0063e5;
+margin-bottom: 12px;
+width: 100%;
+letter-spacing: 1.5px;
+font-size: 18px;
+padding: 16.5px 0;
+border: 1px solid transparent;
+border-radius: 4px;
+
+    &:hover {
+        background-color: #0483ee;
+    }
+`
 export default Login;
